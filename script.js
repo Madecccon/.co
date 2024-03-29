1be88fe3425190419091ea357c68d3b0a2516e17
Madecccon <126336791+Madecccon@users.noreply.github.com>
2024-03-29 07:28:57 +0100

    Add files via upload
    Second commit

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-let menubar = document.querySelector('menu-bar');
-let mynav = document.querySelector('.nav');
-let searchbar = document.querySelector('#search-bar');
-let search = document.querySelector('.search-form');
+document.getElementById("menuIcon").addEventListener("click", function(){
+    var menu = document.getElementById("menu");
+    menu.classList.toggle("active");    
+});
 
-menubar.onclick = () =>{
-    menubar.classList.toggle('far-times')
-    mynav.classList.toggle('active')
-}
-searchbar.onclick = () =>{
-    search.classList.toggle('active')
-}
+document.getElementById("searchButton").addEventListener("click", function(){
+    var searchInput = document.getElementById("searchInput").Value;
+    //perform search based on searchInput, you can replace this with your actual search logic
+    alert("You searched for: " + searchInput);   
+});
